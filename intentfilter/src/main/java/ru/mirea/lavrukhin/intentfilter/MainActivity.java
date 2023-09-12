eb15a688a8b445596479297e2d18d0a842964524
SergSir <126693252+SergSir@users.noreply.github.com>
2023-09-12 15:40:39 +0300

    Update MainActivity.java

--- intentfilter/src/main/java/ru/mirea/lavrukhin/intentfilter/MainActivity.java
+++ intentfilter/src/main/java/ru/mirea/lavrukhin/intentfilter/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, "Махоркин Григорий Юрьевич");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Лаврухин Сергей Алексеевич");
         startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
     }
 }
